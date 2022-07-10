c359bfda9712763a5d79ab44b3ec4f201c146cc7
yurinogueira <yurinogueira@id.uff.br>
2022-07-10 01:08:35 -0300

    chore(settings): add middleware

--- src/api/settings.py
+++ src/api/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "corsheaders.middleware.CorsPostCsrfMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
